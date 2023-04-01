df5198bede31af4aad00199f81808e7bb014d868
MasOOd.KazEmi <masoodkazemii94@gmail.com>
2023-04-01 09:19:38 +0330

    Fix prefix url

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,7 +45,7 @@ class Handler extends ExceptionHandler
         $this->renderable(function (NotFoundHttpException $e, $request) {
             if ($request->is('api/*')) {
                 return response()->json([
-                    'message' => 'Record not found.'
+                    'message' => 'Not Found.'
                 ], 404);
             }
         });
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,7 +28,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
+                ->prefix('vehicle-management/api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
